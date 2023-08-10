51bc5d8d1f073cc1401791ad961c293ba7935928
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-08-10 11:01:45 -0300

    Add files via upload

---
+++ Configurando o Xcode 14/testeviewcode2/testeviewcode2/Login/Screen/LoginScreen.swift
@@ -0,0 +1,27 @@
+//
+//  LoginScreen.swift
+//  testeviewcode2
+//
+//  Created by Lucas Neves dos santos pompeu on 10/08/23.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+    
+    lazy var loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
---
+++ Configurando o Xcode 14/testeviewcode2/testeviewcode2/Login/VC/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testeviewcode2
+//
+//  Created by Lucas Neves dos santos pompeu on 10/08/23.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
